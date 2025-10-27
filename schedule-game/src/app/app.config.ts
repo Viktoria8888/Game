ebb9df7c9ad9c3e71c2c8173f209f5bf2f6cacd8
viki <viki88830@gmail.com>
2025-10-27 20:12:54 +0100

    firestore setup

--- schedule-game/src/app/app.config.ts
+++ schedule-game/src/app/app.config.ts
@@ -1,12 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'schedule-game-d17d5',
+        appId: '1:810073978867:web:570e855740e90eed5a0486',
+        storageBucket: 'schedule-game-d17d5.firebasestorage.app',
+        apiKey: 'AIzaSyAmwP1PdwnsPV_bvGFUKn1djZcXnZHaN-Q',
+        authDomain: 'schedule-game-d17d5.firebaseapp.com',
+        messagingSenderId: '810073978867',
+        measurementId: 'G-QZGSLLZFZB',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
